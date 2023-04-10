1974a34803869773839961abb3187beb9b37ed57
Neurs <31346263+Neurs12@users.noreply.github.com>
2023-04-10 14:33:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", (_) => {
+    let parallax = document.getElementById("scroll-parallax");
+
+    var layers = [];
+    for (var i = 0; i < 9; i++) {
+        layers.push(document.getElementById(`l${i}`));
+    }
+
+    parallax.addEventListener("scroll", function() {
+        let offset = parallax.scrollTop;
+        for (var i = 0; i < 9; i++) {
+            layers[i].style.top = `${offset * (i+1) / 30}px`;
+        }
+    })
+
+    parallax.scrollTo(0, parallax.scrollHeight);
+
+    function scrollTo(element, to, duration) {
+        Math.easeInOutQuad = function(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+        var start = element.scrollTop,
+            change = to - start,
+            currentTime = 0,
+            increment = 5;
+        var animateScroll = function() {
+            currentTime += increment;
+            var val = Math.easeInOutQuad(currentTime, start, change, duration);
+            element.scrollTop = val;
+            if (currentTime < duration) {
+                setTimeout(animateScroll, increment);
+            }
+        };
+        animateScroll();
+    }
+
+    scrollTo(parallax, 0, 1250);
+});
